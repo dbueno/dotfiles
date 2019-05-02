bce553ab537b6236ebbb6308490a1fe4072e4206
Denis Bueno <denbuen@sandia.gov>
2019-05-02 14:13:35 -0600

    update

--- env.sh
+++ env.sh
@@ -1,10 +1,11 @@
-
+# usage:
+# source <this> puts Debug euforia into path
+# source <this> Release
 
 function add_to_path {
   path="$1"
   # Checks f or the presence of the string in PATH before adding
   [ -d "$path" ] && [[ $PATH != *"$path"* ]] && PATH="$conf:$PATH"
-
 }
 
 conf="$HOME/work/inprogress/fmcad19/code"
