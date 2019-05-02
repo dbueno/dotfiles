2104d86a9d2951bf5bff148ab262e0f608ba1aa0
Denis Bueno <dbueno@gmail.com>
2019-05-02 14:12:32 -0600

    initial rev

---
+++ env.sh
@@ -0,0 +1,18 @@
+
+
+function add_to_path {
+  path="$1"
+  # Checks f or the presence of the string in PATH before adding
+  [ -d "$path" ] && [[ $PATH != *"$path"* ]] && PATH="$conf:$PATH"
+
+}
+
+conf="$HOME/work/inprogress/fmcad19/code"
+add_to_path "$conf"
+
+flavor="Debug"
+if test -n "$1"; then
+  flavor="$1"
+fi
+euforia_path="$HOME/work/inprogress/euforia/vmt4/code/euforia/build/$flavor/bin"
+add_to_path "$euforia_path"
