220e85143d2544b718889600ea1ad1bebe07ad00
Denis Bueno <dbueno@gmail.com>
2019-08-01 10:09:32 -0600

    add 1/3rds binding to row of keys

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -4,8 +4,14 @@ units = {
   left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
   left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
   left50        = { x = 0.00, y = 0.00, w = 0.4999, h = 1.00 },
+  -- i think x and y are the top-left of the desktop
+  -- w is the width in pct
   left63        = { x = 0.00, y = 0.00, w = 0.62999, h = 1.00 },
   right37       = { x = 0.63, y = 0.00, w = 0.36999, h = 1.00 },
+  -- these three can give me a layout of three windows by column
+  left33        = { x = 0.00, y = 0.00, w = 0.32999, h = 1.00 },
+  mid33        = { x = 0.33, y = 0.00, w = 0.32999, h = 1.00 },
+  right33        = { x = 0.66, y = 0.00, w = 0.32999, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
   bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
   upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
@@ -28,6 +34,10 @@ mash = { 'shift', 'ctrl', 'cmd' }
 -- when i hit j, it produces h because of dvorak
 hs.hotkey.bind(mash, 'h', function() hs.window.focusedWindow():move(units.left63,     nil, true) end)
 hs.hotkey.bind(mash, 'n', function() hs.window.focusedWindow():move(units.right37,     nil, true) end)
+-- 'u', 'i', and 'o' will layout three windows perfectly taking up a third of the screen, left-to-right
+hs.hotkey.bind(mash, 'g', function() hs.window.focusedWindow():move(units.left33, nil, true) end)
+hs.hotkey.bind(mash, 'c', function() hs.window.focusedWindow():move(units.mid33, nil, true) end)
+hs.hotkey.bind(mash, 'r', function() hs.window.focusedWindow():move(units.right33, nil, true) end)
 -- the h key in dvorak produces d
 -- hs.hotkey.bind(mash, 'd', function() hs.window.focusedWindow():move(units.left70,     nil, true) end)
 -- hs.hotkey.bind(mash, 'k', function() hs.window.focusedWindow():move(units.top50,      nil, true) end)
@@ -63,12 +73,12 @@ hs.hotkey.bind(mash, "down", function()
   win:setFrame(f)
 end)
 -- Maximize the height of the window
-hs.hotkey.bind(mash, "c", function()
-  local win = hs.window.focusedWindow()
-  local sz = win:size()
-  sz.h = win:screen():frame().h
-  win:setSize(sz)
-end)
+-- hs.hotkey.bind(mash, "c", function()
+--   local win = hs.window.focusedWindow()
+--   local sz = win:size()
+--   sz.h = win:screen():frame().h
+--   win:setSize(sz)
+-- end)
 
 -- hs.hotkey.bind(mods, 'down', createMoveWindow(units.bottom))
 -- hs.hotkey.bind(mods, 'left', createMoveWindow(units.left))
