f3d2851e7eb9e3ad551f269cedd48a503b8fc156
Denis Bueno <denbuen@sandia.gov>
2021-07-23 15:20:15 -0600

    working config

--- home.nix
+++ home.nix
@@ -1,9 +1,18 @@
 { config, pkgs, ... }:
+
 let
+  myName = "Denis Bueno";
   nixFlakes =
     (pkgs.writeShellScriptBin "nixFlakes" ''
-      exec ${pkgs.nixUnstable}/bin/nix --experimental-features "nix-command flakes" "$@"
+        exec ${pkgs.nixUnstable}/bin/nix --experimental-features "nix-command flakes" "$@"
     '');
+
+  myScripts = [
+    (pkgs.writeShellScriptBin "ifnewer" (builtins.readFile ./ifnewer.sh))
+    (pkgs.writeShellScriptBin "onchange" (builtins.readFile ./onchange.sh))
+    (pkgs.writeShellScriptBin "wtf" (builtins.readFile ./wtf))
+    (pkgs.writeShellScriptBin "google" (builtins.readFile ./google))
+  ];
 in
 
 {
@@ -12,8 +21,8 @@ in
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "dbueno";
-  home.homeDirectory = "/Users/dbueno";
+  home.username = "denbuen";
+  home.homeDirectory = "/Users/denbuen";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -25,27 +34,338 @@ in
   # changes in each release.
   home.stateVersion = "21.11";
 
+  programs.man.enable = true;
+
+  programs.bat = {
+    enable = true;
+  };
+
+  programs.dircolors = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = false;
+    enableFishIntegration = false;
+  };
+
+  programs.fzf = {
+    enable = true;
+    defaultCommand = ''rg --iglob "!/_opam" --iglob "!/_build" --iglob "!*.o" --files --hidden'';
+  };
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     nix-direnv.enableFlakes = true;
   };
 
-  # My packages
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = myName;
+    userEmail = "dbueno" + "@" + "gmail.com";
+    ignores = [ "*.swp" "*.swo" ];
+    lfs = { enable = true; };
+
+    aliases = {
+      d = "diff";
+      di = "diff --cached";
+      pa = "push --all";
+      co = "checkout";
+      st = "status";
+      ci = "commit";
+      push-it-real-good = "push -f";
+      dt = "difftool";
+    };
+
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      core = {
+        fsyncobjectfiles = "true";
+      };
+      push = { default = "simple"; };
+      color = {
+        interactive = "auto";
+        diff = "auto";
+      };
+    };
+  };
+
+  programs.htop = {
+    enable = true;
+  };
+
+  programs.nix-index = {
+    enable = true;
+  };
+
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_family = "SF Mono Medium";
+      font_size = "13.0";
+      enabled_layouts = "tall:bias=50;full_size=1;mirrored=false,fat,stack";
+      confirm_os_window_close = 1;
+    };
+    keybindings = {
+      "ctrl+p" = "scroll_page_up";
+      "ctrl+n" = "scroll_page_down";
+    };
+    extraConfig = builtins.readFile ./kitty-nord-theme.conf;
+  };
+
+  programs.readline = {
+    enable = true;
+    variables = {
+      bell-style = "visible";
+      # Immediately show all TAB-completions -- don't require two TABs.
+      # show-all-if-ambiguous = true;
+      # Show status of completed filenames, like ls -F does.
+      visible-stats = true;
+      completion-ignore-case = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    historySize = 1000000;
+    # The & removes dups; [ ]* ignores commands prefixed with spaces.  Other
+    # commands, like job control and ls'ing are also ignored.
+    historyIgnore = [ "\"&\"" "\"[ ]*\"" "exit" "pwd" "\"[bf]g\"" "no" "lo" "lt" "pd" "c" "a" "aa" "s" "ss" ];
+
+    shellOptions = [
+      # Correct transpositions and other minor details from 'cd DIR' command.
+      "cdspell"
+      # Include hidden files in glob (*).
+      "dotglob"
+      # Extra-special pattern matching in the shell.
+      "extglob"
+      # Flatten multiple-line commands into the same history entry.
+      "cmdhist"
+      # Don't complete when I haven't typed anything.
+      "no_empty_cmd_completion"
+      # Let me know if I shift stupidly.
+      "shift_verbose"
+      # With this, cd $HOME/<TAB> expands the variable instead of escaping it.
+      # Ideally, bash would just leave it alone.
+      "direxpand"
+    ];
+
+    sessionVariables = {
+      LANG = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      EDITOR = "vim";
+      ED = "vim";
+      # For Linux, pretty colors.
+      # LS_COLORS = "di=00;36;40:ln=00;35:ex=00;31";
+      # For mac, pretty colors. -- but see also dircolors above
+      LSCOLORS = "gxfxcxdxbxegedabagacad";
+      # When I type 'cd somewhere', if somewhere is relative, try out looking into all
+      # the paths in $CDPATH for completions.  This can speed up common directory
+      # switching.
+      #
+      # The . is here so that if I type cd <dir> it goes to curdir first
+      CDPATH = ".:~/work/inprogress";
+    };
+
+    shellAliases =
+      (if pkgs.stdenv.isDarwin then {
+        a = let
+            cmd = pkgs.writeShellScriptBin "my-ls" ''
+              num_files=$(ls -1 "$@" 2>/dev/null | wc -l | sed 's/[[:space:]]//g')
+              cutoff=20
+              if [ $num_files -gt $cutoff ]; then
+                CLICOLOR_FORCE=1 ls -lFGtrh "$@" | tail -n $cutoff
+                printf "[showing 20 of %d files]\n" $num_files
+              else
+                CLICOLOR_FORCE=1 ls -lFGtrh "$@"
+              fi
+            '';
+          in
+          "${cmd}/bin/my-ls";
+        aa="ls -lFGtrh";
+        mk="make -j$(sysctl -a | grep ^hw[.]ncpu | cut -d' ' -f2)";
+        lldb="PATH=/usr/bin:$PATH lldb";
+      } else {
+        a = let
+            cmd = pkgs.writeShellScriptBin "my-ls" ''
+              num_files=$(ls -1 "$@" 2>/dev/null | wc -l | sed 's/[[:space:]]//g')
+              cutoff=20
+              if [ $num_files -gt $cutoff ]; then
+                ls -lF --color -trh "$@" | tail -n $cutoff
+                printf "[showing 20 of %d files]\n" $num_files
+              else
+                ls -lF --color -trh "$@"
+              fi
+            '';
+          in
+          "{cmd}/bin/my-ls";
+        aa="ls -lF --color -trh";
+      })
+    // {
+      # there's always a story behind aliases like these
+      rm = "rm -i";
+      c = "clear";
+      pd = "cd \"$OLDPWD\"";
+      # Evaluates to an iso-conformant date.  The iso-conformance is good because
+      # lexicographic order coincides with date order.  'nows' just has seconds and
+      # is also iso-conformant.
+      now = "date '+%Y-%m-%dT%H%M'";
+      nows = "date '+%Y-%m-%dT%H%M%S'";
+      today = "date '+%Y-%m-%d'";
+
+      average = "${pkgs.R}/bin/Rscript -e 'd<-scan(\"stdin\", quiet=TRUE)' -e 'summary(d)'";
+
+      # Displays an image (png) in the terminal
+      icat = "kitty +kitten icat --align=left";
+      # Displays an SVG in the terminal
+      isvg = "${pkgs.librsvg}/bin/rsvg-convert | icat";
+      # Displays a DOT graph in the terminal
+      # see ratio="compress" or unflatten | idot
+      idot = ''${pkgs.graphviz}/bin/dot -Gbgcolor=transparent -Nfontcolor=#2E3440 -Nstyle=filled \
+        -Nfillcolor=#D8DEE9 -Ecolor=#5E81AC -Efontcolor=#8FBCBB -Nfontname=Helvetica -Efontname=Helvetica \
+        -Nfontsize=11 -Efontsize=10 -Gratio=compress -Tsvg | isvg'';
+      # XXX where is hb-view
+      hb-feat =
+        let cmd = pkgs.writeShellScriptBin "hb-feat" ''
+          # from @thingskatedid
+          # otfinfo --features <file.otf> to see features
+          # Default color is black so the sed changes it to whitish (nord palette).
+          ${pkgs.harfbuzz}/bin/hb-view --features="$2" -O svg "$1" "$3" | \
+              grep -v '<rect' | \
+              sed 's/<g style="fill:rgb(0%,0%,0%);fill-opacity:1;">/<g style="fill:#ECEFF4">/' | \
+              ${pkgs.librsvg}/bin/rsvg-convert | \
+              ${pkgs.imagemagick}/bin/convert -trim -resize '25%' - - | \
+              kitty +kitten icat --align=left
+              '';
+        in
+        "${cmd}/bin/hb-feat";
+
+        # git aliases
+      s = let
+          # Prints status without untracked files
+          cmd = pkgs.writeShellScriptBin "my-git-status" ''
+            git status -uno || exit 0
+            # Prints only a count summary of untracked files
+            git status --short | awk '/[?][?]/ { c += 1 } END { if (c > 0) { printf("\n... and %s untracked files\n", c) } }' || exit 0
+            '';
+        in
+        "${cmd}/bin/my-git-status";
+      ss = "git status";
+      gp = "git pull";
+      ga = "git add -p";
+      gci = "git commit";
+      # print git directory, toplevel of current repo
+      pgd = "git rev-parse --show-toplevel";
+
+      # Greps and displays with less, with colors
+      lrg = ''rg --line-buffered --pretty "$@" | less -R'';
+      # Greps and fuzzy selects
+      frg = ''rg "$@" | fzf'';
+      ssh = let
+        cmd = pkgs.writeShellScriptBin "my-ssh" ''
+          if [[ "$TERM" = *kitty ]]; then
+              env TERM=xterm-256color ssh "$@"
+          else
+              env ssh "$@"
+          fi
+          '';
+      in
+        "${cmd}/bin/my-ssh";
+
+      make_cpptags = "${pkgs.universal-ctags}/bin/ctags --c++-kinds=+pf --c-kinds=+p --fields=+imaSft --extra=+q -Rnu";
+    };
+
+    # Settings for interactive shells
+    # .bashrc is executed for interactive non-login shells
+    bashrcExtra = builtins.readFile ./bashrc_extra;
+
+    profileExtra = ''
+      [[ -e "$HOME/.bash_profile_local" ]] && source "$HOME/.bash_profile_local"
+      history -a
+    '';
+  };
+
+  home.file = {
+    ".bash_completion".source = ./.bash_completion;
+    ".tmux-linux".source = ./.tmux-linux;
+    ".hammerspoon/init.lua".source = ./hammerspoon.lua;
+    ".gdbinit".source = ./.gdbinit;
+    ".ghci".source = ./.ghci;
+    ".lynxrc".source = ./.lynxrc;
+<<<<<<< HEAD
+=======
+    ".config/automate-everything/repos".source = ./repos.conf;
+>>>>>>> 223c2a6 (move things out of repo)
+  };
+
+  programs.tmux = {
+    enable = true;
+    extraConfig = builtins.readFile ./.tmux.conf;
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "*" = { identitiesOnly = true; };
+      "denisbueno.net" = { user = "dbueno"; };
+    }
+    // (import ./ssh_extra_blocks.nix);
+  };
+
+  #Example of vim plugin from git
+    # context-vim = pkgs.vimUtils.buildVimPlugin {
+    #   name = "context-vim";
+    #   src = pkgs.fetchFromGitHub {
+    #   owner = "wellle";
+    #   repo = "context.vim";
+    #   rev = "e38496f1eb5bb52b1022e5c1f694e9be61c3714c";
+    #   sha256 = "1iy614py9qz4rwk9p4pr1ci0m1lvxil0xiv3ymqzhqrw5l55n346";
+    #   };
+    #   };
+
+  programs.vim = {
+    enable = true;
+    settings = {
+      # keep buffers around when they are not visible
+      hidden = true;
+      # ignore case except mixed lower and uppercase in patterns
+      smartcase = true;
+      ignorecase = true;
+      modeline = true;
+      mouse = "a";
+      number = true;
+      expandtab = true;
+      # default shiftwidth to 4
+      shiftwidth = 4;
+    };
+    plugins = with pkgs.vimPlugins; [
+      fzf-vim
+      nord-vim
+      vim-ocaml
+      vim-fugitive
+      vim-surround
+      vim-vinegar
+      vim-unimpaired
+      verilog_systemverilog-vim
+      vimtex
+      vim-nix
+      vim-pathogen
+      vim-commentary
+      vim-repeat
+    ];
+    extraConfig = builtins.readFile ./vimrc_extra;
+  };
+
   home.packages = with pkgs; [
     bashInteractive_5
-    git
-    git-lfs
-    kitty
-    myVim
-    fzf
     ripgrep
-    bash-completion
-    nix-bash-completions
+    bash-completion nix-bash-completions
     graphviz
     wget
     parallel
-    htop
     nixFlakes
-  ];
+    ncdu
+    zip unzip p7zip
+  ]
+  ++ myScripts;
 }
