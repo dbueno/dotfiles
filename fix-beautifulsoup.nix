ab15358f769bc3d273e77af5dd4de0324c7dbd06
Denis Bueno <dbueno@gmail.com>
2021-09-17 14:29:22 -0600

    fix for current issues

---
+++ fix-beautifulsoup.nix
@@ -0,0 +1,22 @@
+self: super:
+
+let
+  libxml2-with-iconv = super.libxml2.overrideAttrs (attrs: {
+    propagatedBuildInputs = attrs.propagatedBuildInputs ++ self.lib.optional self.stdenv.isDarwin self.libiconv;
+  });
+in {
+  # Fix for broken python beautifulsoup... change the stdenv. Perfect.
+  # libxml2 = libxml2-with-iconv;
+
+  # Fix for broken python beautifulsoup...
+  python3 = super.python3.override {
+    # Careful, we're using a different self and super here!
+    packageOverrides = self: super: {
+      beautifulsoup4 = super.beautifulsoup4.override {
+        lxml = super.lxml.override { libxml2 = libxml2-with-iconv; };
+      };
+      # lxml = super.lxml.override { libxml2 = libxml2-with-iconv; };
+    };
+  };
+  python3Packages = self.python3.pkgs;
+}
