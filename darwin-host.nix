bd2be19bd113df222c63da477fa65c68c3c29061
Denis Bueno <dbueno@gmail.com>
2022-05-04 07:57:24 -0600

    move fsync settings to darwin-host and update

--- darwin-host.nix
+++ darwin-host.nix
@@ -3,8 +3,11 @@
   programs.vim = {
     packageConfigurable = pkgs.vim_configurable.override { darwinSupport = true; guiSupport = false; };
   };
+
   programs.kitty.settings = {
     font_family = "SF Mono Medium";
     font_size = "13.0";
   };
+
+  programs.git.extraConfig.core.fsync = "all";
 }
