d178b69de479f8182c51c1b7811a07ae0b0a3f5c
Denis Bueno <dbueno@gmail.com>
2023-01-13 15:41:51 -0700

    next try

--- overlays/kitty.nix
+++ overlays/kitty.nix
@@ -1,14 +1,24 @@
 result: prev: {
-  kitty = prev.kitty.overridePythonAttrs (attrs: with attrs; {
-    version = "0.26.2";
-    name = "kitty-0.26.2";
-    src = result.fetchFromGitHub {
-      owner = "kovidgoyal";
-      repo = "kitty";
-      rev = "v0.26.2";
-      sha256 = "sha256-IqXRkKzOfqWolH/534nmM2R/69olhFOk6wbbF4ifRd0=";
-    };
-    meta.broken = false;
-  });
+  kitty =
+    let
+      kitty = prev.kitty.override { };
+    in
+    kitty.overridePythonAttrs (attrs: with attrs; {
+      buildInputs = buildInputs ++ (with result.darwin.apple_sdk_11_0.frameworks; [ SecurityFoundation ]);
+      meta = meta // { broken = false; };
+    });
+
+  # kitty = prev.kitty.overridePythonAttrs (attrs: with attrs; {
+  #   version = "0.25.2";
+  #   name = "kitty-0.25.2";
+  #   src = result.fetchFromGitHub {
+  #     owner = "kovidgoyal";
+  #     repo = "kitty";
+  #     rev = "v0.25.2";
+  #     # sha256 = result.lib.fakeSha256; #"sha256-IqXRkKzOfqWolH/534nmM2R/69olhFOk6wbbF4ifRd0=";
+  #     sha256 = "o/vVz1lPfsgkzbYjYhIrScCAROmVdiPsNwjW/m5n7Us=";
+  #   };
+    # meta = meta // { broken = false; };
+  # });
 }
 
