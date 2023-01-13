1f239ab92a5b03c657ed71d8a060375892065e3f
Denis Bueno <dbueno@gmail.com>
2023-01-13 09:22:25 -0700

    fix overlay

--- overlays/kitty.nix
+++ overlays/kitty.nix
@@ -1,12 +1,14 @@
 result: prev: {
   kitty = prev.kitty.overrideAttrs (attrs: with attrs; {
     version = "0.26.2";
-    src = fetchFromGitHub {
+    name = "kitty-0.26.2";
+    src = result.fetchFromGitHub {
       owner = "kovidgoyal";
       repo = "kitty";
       rev = "v0.26.2";
       sha256 = "sha256-IqXRkKzOfqWolH/534nmM2R/69olhFOk6wbbF4ifRd0=";
     };
+    meta.broken = false;
   });
 }
 
