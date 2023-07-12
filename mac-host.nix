76c1323a6e87682272082b12ce765ec8c33fb2e8
Denis Bueno <dbueno@gmail.com>
2023-07-12 12:54:50 -0600

    hack font

--- mac-host.nix
+++ mac-host.nix
@@ -3,7 +3,7 @@ let
   hammerspoon = pkgs.callPackage ./pkgs/hammerspoon/default.nix {};
 in
 {
-  imports = [ ./fonts/font-intel-one-mono.nix ];
+  imports = [ ./fonts/font-hack.nix ];
 
   nixpkgs.overlays = [ ];
 
