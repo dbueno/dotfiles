7b3d1b1f7a2e8854aedad1c5ee5d2dba81f194d6
Denis Bueno <dbueno@gmail.com>
2023-09-20 14:46:43 -0600

    other neovim settings

--- neovim.nix
+++ neovim.nix
@@ -98,6 +98,14 @@ in {
     ];
     extraConfig = builtins.readFile ./config/nvim/vimrc
     + ''
+
+      set hidden
+      set smartcase
+      set ignorecase
+      set modeline
+      set number
+      set expandtab
+      set shiftwidth=2
       set completeopt=menuone,noinsert,noselect
 
       set cmdheight=2
@@ -110,21 +118,6 @@ in {
 
 
     '';
-    extraLuaConfig = ''
-
-      require'nvim-treesitter.configs'.setup {
-        highlight = {
-          enable = true,              -- false will disable the whole extension
-          disable = { "rust" },  -- list of language that will be disabled
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-      }
-
-      require'lspconfig'.pyright.setup{}
-    '';
+    extraLuaConfig = builtins.readFile ./config/nvim/init.lua;
   };
 }
