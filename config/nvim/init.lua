9e9c30d25689ad2fb50d77698bc0f96b370c6487
Denis Bueno <dbueno@gmail.com>
2024-03-27 15:16:12 -0600

    nvim: lsp go to type definition binding

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -25,5 +25,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
   end,
 })
