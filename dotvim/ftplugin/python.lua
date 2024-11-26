57ffa67341b6f2ea62ed340c8f0f2f852c99f5d7
Denis Bueno <dbueno@gmail.com>
2024-11-26 07:30:26 -0700

    Fix black formatting

---
+++ dotvim/ftplugin/python.lua
@@ -0,0 +1,17 @@
+function Black()
+  local buf = vim.api.nvim_get_current_buf()
+  local path = vim.api.nvim_buf_get_name(buf)
+  local modified = vim.api.nvim_buf_get_option(buf, 'modified')
+  vim.fn.system('black ' .. path)
+  vim.cmd('edit!')
+end
+
+vim.api.nvim_create_user_command('Black', Black, {})
+--vim.cmd('command! Black if &modified | echoerr "Buffer is not saved! Please save before formatting." | else | lua Black() | edit! | endif')
+
+vim.cmd([[
+  augroup pyBlack
+    autocmd!
+    autocmd BufWritePost *.py execute ':Black'
+  augroup END
+]])
--- dotvim/ftplugin/python.vim
+++ dotvim/ftplugin/python.vim
@@ -4,10 +4,3 @@ setlocal fdl=8
 
 nnoremap <buffer> <Leader>is o# -----------------------------------------------------------------------------<CR>
 nnoremap <buffer> gF :Black<CR>
-
-command! Black if &modified | echoerr "Buffer is not saved! Please save before formatting." | else | execute '!black %' | edit! | endif
-
-augroup pyBlack
-  autocmd!
-  autocmd BufWritePost *.py execute ':Black'
-augroup END
