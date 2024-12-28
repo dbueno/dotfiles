d9eabed9c9c2d78d20b98f5134d9fa4c50f0a0b9
Denis Bueno <dbueno@gmail.com>
2024-12-28 10:39:19 -0700

    Ghostty config

--- shell.nix
+++ shell.nix
@@ -272,6 +272,7 @@ in
   xdg = {
     enable = true;
     configFile."ghostty/config".source = ./config/ghostty-config;
+    configFile."ghostty/themes/tokyonight-night".source = ./config/ghostty/tokyonight-night;
     configFile."mutt/muttrc".source = ./.muttrc;
     configFile."wezterm/wezterm.lua".source = ./wezterm/wezterm.lua;
     configFile."wezterm/appearance.lua".source = ./wezterm/appearance.lua;
