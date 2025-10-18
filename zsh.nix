6017ac2ea6e4c23348e9fdf24381e52054ac30e9
Denis Bueno <dbueno@gmail.com>
2025-10-18 09:05:09 -0600

    Refactor
    - Add ~/proj to cdpath
    - Refactor zsh stuff so most of it is in simply init files, not nix

--- zsh.nix
+++ zsh.nix
@@ -5,7 +5,6 @@
   ...
 }:
 {
-  imports = [ (import ./shell-aliases.nix) ];
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -24,6 +23,10 @@
       GRAPHVIZ_DOT = "${pkgs.graphviz}/bin/dot";
       RSVG_CONVERT = "${pkgs.librsvg}/bin/rsvg-convert";
       PAGER = "less -SXRF";
+      COREUTILS_LS = "${pkgs.coreutils}/bin/ls";
+      COREUTILS_SHUF = "${pkgs.coreutils}/bin/shuf";
+      UNIVERSAL_CTAGS = "${pkgs.universal-ctags}/bin/ctags";
+      HM_XDG_CONFIG_HOME = "${config.xdg.configHome}";
     };
 
     history = {
@@ -33,13 +36,12 @@
         "&\n"
         "exit\n"
         "pwd\n"
+        "p\n"
         "fg\n"
         "bg\n"
-        "pd\n"
         "a\n"
         "aa\n"
         "al\n"
-        "g reset*"
         "git reset*"
       ];
       ignoreSpace = true;
@@ -55,106 +57,10 @@
     ];
 
     initContent = lib.mkMerge [
-      (lib.mkOrder 500 ''
-        # Load nix in case it's not in path
-        [[ ! $(command -v nix) && -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]] && source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-        export LANG="en_US.UTF-8"
-        setopt incappendhistory
-        setopt histsavenodups
-        setopt histexpiredupsfirst
-        setopt hist_no_store # don't store "history" command in history
-        fpath=(${config.xdg.configHome}/zsh/vendor-completions \
-               $fpath)
-      '')
-      ''
-        zshaddhistory() {
-          emulate -L zsh
-          ## uncomment if HISTORY_IGNORE
-          ## should use EXTENDED_GLOB syntax
-          setopt extendedglob
-          [[ $1 != ''${~HISTORY_IGNORE} ]]
-        }
-        setopt pushd_ignore_dups
-        setopt ignoreeof # I hit ctrl-d *all the time*, never to exit
-
-        # case insensitive partial filename completion (??)
-        zstyle ':completion:*' matcher-list \
-          'm:{[:lower:]}={[:upper:]}' \
-          '+r:|[._-]=* r:|=*' \
-          '+l:|=*'
-
-        # alias average="Rscript -e 'd<-scan(\"stdin\", quiet=TRUE)' -e 'cat(min(d), max(d), median(d), mean(d), sep=\"\n\")'"
-
-        # Greps and displays with less, with colors
-        function rgl {
-            rg --line-buffered --pretty "$@" | less -R
-        }
-        # Greps and fuzzy selects
-        function rgfzf {
-            rg "$@" | fzf
-        }
-        function sortinplace {
-          sort "$1" | sponge "$1"
-        }
-
-        # Setting and jumping to topic windows
-        function list-topics {
-          kitten @ ls | jq -r ".[].tabs[].windows[].user_vars.topic | select(. != null)"
-        }
-        function set-topic {
-          if test -z "$1"; then
-            echo "error: need topic argument"
-            return 1
-          fi
-          kitten @ set-user-vars topic="$1"
-        }
-        function topics {
-          f=`mktemp -t swwin XXXXXX`
-          list-topics > "$f"
-          topic=$(cat "$f" | fzf | tr -d "\n")
-          rm -f "$f"
-          kitten @ focus-window --match var:topic=$topic
-        }
-        function switch-tab {
-          f=`mktemp -t swtab XXXXXX`
-          list-topics > "$f"
-          topic=$(cat "$f" | fzf | tr -d "\n")
-          rm -f "$f"
-          kitten @ focus-tab --match var:topic=$topic
-        }
-
-        # PS1 settings are for interactive shells (login or no), so they should be
-        # set in .bashrc.
-        # Colors the prompt red if the exit code argument isn't 0.
-        function __colorcode_exit {
-            if test "$1" -eq 0; then
-                print ";"
-                #print "%{%B%};%{%b%}"
-            else
-                print "%{%F{red}%B%};%{%f%b%}"
-            fi
-        }
-        function __colorcode_setps1 {
-            local last_exit="$?"
-            # I tried, at first, setting PS1 in .bashrc alone. But I ran into a problem
-            # where the \[ and \] in __colorcode_exit were being literally printed in
-            # the prompt, instead of interpreted as directives for bash. Setting PS1 in
-            # PROMPT_COMMAND fixes this problem.
-            PS1="$(__colorcode_exit $last_exit) "
-        }
-        mypromptcommand() {
-          PS1="$(__colorcode_exit $?) "
-        }
-        precmd_functions+=(mypromptcommand)
-
-        # Base16 Shell
-        BASE16_SHELL="$HOME/.config/base16-shell/"
-        [ -n "$PS1" ] && \
-            [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-                source "$BASE16_SHELL/profile_helper.sh"
-
-        . $HOME/.zshrc_local
-      ''
+      # This puts some important shell and nix stuff first so the rest of the
+      # shell init can access it
+      (lib.mkOrder 500 (builtins.readFile ./zsh/start))
+      (builtins.readFile ./zsh/rc)
     ];
   };
 
